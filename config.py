c7011dd4fcbb550ba7a9aafc262b14eb286fd39a
Biisal <153633053+biisal@users.noreply.github.com>
2024-04-21 11:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "9176f5b87d53fc4f668e84c4cb477ac8")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6157402161:AAHjE9S3icWRLh4jwBdHY9AfxrsrYzExnNs")
     BOT_SESSION = environ.get("BOT_SESSION", "fjdfjfd")
-    DATABASE_URI = environ.get("DATABASE_URI","mongodb://localhost:27017",)
+    DATABASE_URI = environ.get("DATABASE_URI","mongodb+srv://Xtxmovies123:JalsAwUixJcwxx1Y@cluster0.viml1ha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",)
     DATABASE_NAME = environ.get("DATABASE_NAME", "cluster12")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", "1832810840").split()]
     START_PIC = environ.get("START_PIC", "https://graph.org/file/c3c11dbbef4523ba84183.jpg")
